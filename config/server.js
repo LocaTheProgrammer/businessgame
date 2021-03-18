dc138dbe43f88c79b68115fcd6d14397a6eae856
LocaTheProgrammer <pietro.locatelli00@gmail.com>
2021-03-18 12:40:25 +0100

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '4a28e65c264fabd92142bd73ac492e48'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '89a9b8ba-98ca-4ca0-ba50-ee61eaba820d'
+};
